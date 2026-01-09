69475b7dc290055ec3032b2069bbe84e2d99b9e9
umar <rusumumarfarooq@gmail.com>
2026-01-09 10:17:01 +0530

    Code added to fail

--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.cicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(false);
 	}
 
 }
